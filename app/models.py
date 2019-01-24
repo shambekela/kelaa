6e4e478f9f2aea7a4159cd3464613ecd03038d5a
Toivo Toivo <toivo1996@gmail.com>
2019-01-24 22:33:42 +0200

    Models update

--- app/models.py
+++ app/models.py
@@ -12,20 +12,11 @@ class User(UserMixin, db.Model):
 	uuid = db.Column(db.Integer, unique=True ,index=True)
 	username = db.Column(db.String(255), unique=True, nullable=False)
 	email = db.Column(db.String(255), unique=True)
-	date_joined = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	avatar = db.Column(db.String(200))
-	tokens = db.Column(db.Text)
-	num_of_logins = db.Column(db.Integer, default=1)
-	receiveEmail = db.Column(db.Boolean, default=True)
 	password_hash = db.Column(db.String(255), nullable=True)
-	confirmed = db.Column(db.Boolean, default=False)
-	login_type = db.Column(db.Integer, nullable=False) # 0 - email and 1 - social login
-	profile_type = db.Column(db.Integer, nullable=True) # 0 - student , 1 - Professional , 2- Hobbiest  
+	channels = db.relationship('Channel', backref='creator', lazy=True)
+	trackers = db.relationship('Tracker', backref='user', lazy=True) 
 
 	# set getstarted modal seen
-	def set_getStarted(self, seen):
-		self.get_started = seen
-
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
 
@@ -69,6 +60,54 @@ class User(UserMixin, db.Model):
 		db.session.add(user)
 		db.session.commit()
 		return True
+
+class UserDetails(db.Model):
+	__tablename__ = 'user_detail'
+	id = db.Column(db.Integer, auto_increment=True)
+	user_uuid = db.Column(db.Integer, db.ForeignKey('user.uuid', ondelete="CASCADE"))
+	receive_email = db.Column(db.Boolean, default=True)
+	email_confirmed = db.Column(db.Boolean, default=False)
+	login_type = db.Column(db.Integer, db.ForeignKey('login_type.id'))
+	user_type = db.Column(db.Integer, db.ForeignKey('user_type.id'))
+	date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+	full_name = db.Column(db.String(255), index=True)
+	avatar = db.Column(db.String(200))
+	tokens = db.Column(db.Text)
+
+class Channel(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	key = db.Column(db.Integer, unique=True, index=True)
+	created_on = db.Column(db.DateTime, default=datetime.utcnow)
+	created_by = db.Column(db.Integer, db.ForeignKey('user.uuid'))
+	title = db.Column(db.String(255), nullable=False, index=True)
+	description = db.Column(db.Text, nullable=True)
+	questions = db.relationship('Question', backref='question', cascade="all, delete",lazy=True)
+
+class Question(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	key = db.Column(db.String(128), unique=True)
+	channel_key = db.Column(db.Integer, db.ForeignKey('channel.key', ondelete="CASCADE"))
+	text = db.Column(db.String(255), nullable=False)
+	answer_page = db.Column(db.String(128))
+	answer_text = db.Column(db.Text)
+	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+	favourite = db.Column(db.Boolean, default=False)
+
+class Tracker(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+	user_uuid = db.Column(db.Integer, db.ForeignKey('user.uuid'))
+
+class Activity(db.Model):
+	id = 
+
+
+class LoginType(db.Model):
+	__tablename__ = 'login_type'
+	id = db.Column(db.Integer, primary_key=True)
+	type = db.Column(db.String(128), nullable=False) # google, email, twitter
+	users = db.relationship('UserDetails', backref='logintype', lazy=True)	
+
 
 @login_manager.user_loader
 def load_user(user_id):
